f5f0b8bb33c2fe1c01ef904eb4fce9d6367f5dc2
Sheila Anguiano <sheila.anguiano@gmail.com>
2022-07-31 15:08:01 -0700

    Destructure of actions and players

--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -5,16 +5,12 @@ import Player from './Player';
 const PlayerList = () => {
   return (
     <Consumer>
-      { context => {
+      { ({ players }) => {
 
         return(
           <React.Fragment>
-            {context.players.map( (player, index) =>
+            { players.map( (player, index) =>
             <Player 
-            /* We using this spread operator to pass the name, score 
-            and ID we're getting from each player object via map all 
-            at once as props, that way you don't have to explicitly list each prop name and value */ 
-              {...player}
               key={player.id.toString()} 
               index={index}
                
